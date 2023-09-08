918e4d4d26b33552da8eaa86dfe919ed58c848b2
Thiago Lima <thiagolimadeoliveira18@gmail.com>
2023-09-08 20:49:42 -0300

    GetPokemonById function

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -14,6 +14,12 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
 
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 
+    pokemon.height = pokeDetail.height
+    pokemon.weight = pokeDetail.weight
+    pokemon.attack = pokeDetail.stats[1].base_stat
+    pokemon.defense = pokeDetail.stats[2].base_stat
+    pokemon.speed = pokeDetail.stats[5].base_stat
+
     return pokemon
 }
 
@@ -32,4 +38,13 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
+}
+
+pokeApi.getPokemonById = (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}/`
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+        .then((pokemonObject) => console.log(pokemonObject))
 }
